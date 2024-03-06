68cc11122ca371c92e24823da5c780ca5f90579d
nhuducbn123456 <nhuducbn123456@gmail.com>
2024-03-06 18:28:12 +0700

    CreateProject

---
+++ mvcmovie/Models/Person.cs
@@ -0,0 +1,7 @@
+namespace mvcmovie.Model;
+public class Person
+{
+    public required string PersonId {get;set;}
+    public required string FullName {get;set;}
+    public required string Address {get;set;}
+}
