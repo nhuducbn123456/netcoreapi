d21056a2e7e0a6e452622c134065733632dc85a6
nhuducbn123456 <nhuducbn123456@gmail.com>
2024-03-11 18:40:22 +0700

    CreateProject

--- newapp/Models/Person.cs
+++ newapp/Models/Person.cs
@@ -18,5 +18,22 @@ namespace NewApp.Models
         {
             System.Console.WriteLine("{0} - {1} - {2} tuoi ",FullName,Address,Age);
         }
+        public void Display(string ten, int tuoi)
+        {
+            System.Console.WriteLine("Sinh vien {0} - {1} tuoi", ten, tuoi);
+        }
+        public void Display(string ten, string diaChi)
+        {
+            System.Console.WriteLine("Sinh vien {0} - {1}", ten, diaChi);
+        }
+        public int GetYearOfBirth(int age)
+        {
+            int yearOfBirth = 2023 - age;
+            return yearOfBirth;
+        }
+        public void Display2(string ten,int tuoi)
+        {
+            System.Console.WriteLine("Sinh vien {0} - {1} tuoi ", ten, tuoi);
+        }
     }
 }
--- newapp/Program.cs
+++ newapp/Program.cs
@@ -5,14 +5,11 @@ public class Program
     private static void Main(string[] args)
     {
         //khởi tạo 2 đối tượng
-        Person ps1 = new Person();
-        Person ps2 = new Person();
+        Person ps = new Person();
         //gắn giá trị cho thuộc tính đối tượng ps1
-        ps1.FullName = "Nguyen Van A";
-        ps1.Address = "Ha Noi";
-        ps1.Age = 18;
+        string str = "Nguyen Van A";
+        int a = 22;
         //phương thức thực hiện thông tin
-        ps1.Display();
-        ps2.Display();
+        Console.WriteLine("{0} sinh nam {1}", str, ps.GetYearOfBirth(a));
     }
 }
