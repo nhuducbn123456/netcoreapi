72dcaeb2a135722fca1e5612aff1846afcedfcf5
nhuducbn123456 <nhuducbn123456@gmail.com>
2024-03-11 18:03:10 +0700

    CreateProject

--- newapp/Program.cs
+++ newapp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NewApp.Models;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        //khởi tạo 2 đối tượng
+        Person ps1 = new Person();
+        Person ps2 = new Person();
+        //gắn giá trị cho thuộc tính đối tượng ps1
+        ps1.FullName = "Nguyen Van A";
+        ps1.Address = "Ha Noi";
+        ps1.Age = 18;
+        //phương thức thực hiện thông tin
+        ps1.Display();
+        ps2.Display();
+    }
+}
